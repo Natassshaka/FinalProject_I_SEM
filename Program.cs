9b321124fa98243acd17b8f5bedd274025b397fb
Natalie <tash151@yandex.ru>
2022-08-02 21:52:18 +0300

    Add metod

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Arr1 = new string[4] {"HELLO", "2","world", ":-)"};
+string[] Arr2 = new string[Arr1.Length];
+void SecondArrayWithIF(string[] Array1, string[] Array2)
+{
+    int count = 0;
+    for (int i = 0; i < Array1.Length; i++)
+    {
+    if(Arr1[i].Length <= 3)
+        {
+        Arr2[count] = Array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(Arr1, Arr2);
+PrintArray(Arr2);
